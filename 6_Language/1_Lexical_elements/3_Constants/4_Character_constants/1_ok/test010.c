540488c581cc9ccaf8a7b5f89bd9a5f63660ad40
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-03-11 16:42:30 +0900

    add 10,11

---
+++ 6_Language/1_Lexical_elements/3_Constants/4_Character_constants/1_ok/test010.c
@@ -0,0 +1,10 @@
+#include <wchar.h>
+#include <stdio.h>
+
+int main()
+{
+  wchar_t wc = L'怒';
+  printf("%x\n", wc);
+  return 0;
+}
+
---
+++ 6_Language/1_Lexical_elements/3_Constants/4_Character_constants/1_ok/test011.c
@@ -0,0 +1,10 @@
+#include <wchar.h>
+#include <stdio.h>
+
+int main()
+{
+  wchar_t wc = L'倍';
+  printf("%x\n", wc);
+  return 0;
+}
+
